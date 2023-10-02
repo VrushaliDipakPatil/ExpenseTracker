9fe36fa280d7a29c754e59f696874899ddf4e7b6
patil-vrushali <Vrushali.p@tecxlabs.com>
2023-10-02 22:04:30 +0530

    project added

---
+++ src/store/index.js
@@ -0,0 +1,14 @@
+
+import { configureStore} from '@reduxjs/toolkit';
+
+import authReducer from './authSlice'
+import expenseReducer from './expenseSlice'
+
+const store = configureStore({
+    reducer : { auth:authReducer, expenses:expenseReducer}
+});
+
+
+
+
+export default store;
